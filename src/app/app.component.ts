87a25002d3f99959fe93bf276f97f6ed60cf6068
Vicente Benavides <vicente.benavides0@gmail.com>
2022-10-09 13:43:26 -0500

    añadiendo login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 
-import { Product } from './models/product.model';
+import { AuthService } from './services/auth.service';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   imgParent = '';
   showImg = true;
+  token = '';
 
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService
+  ) {}
 
   onLoaded(img: string) {
     console.log('log padre', img);
@@ -18,5 +24,17 @@ export class AppComponent {
 
   toggleImg() {
     this.showImg = !this.showImg;
+  }
+
+  createUser() {
+    this.usersService
+      .create({
+        name: 'vicente',
+        email: 'vicente@gmail.com',
+        password: '123123',
+      })
+      .subscribe((rta) => {
+        console.log(rta);
+      });
   }
 }
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,23 +1,31 @@
+import { switchMap } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
 
-import { StoreService } from '../../services/store.service'
+import { StoreService } from '../../services/store.service';
+import { AuthService } from '../../services/auth.service';
+import { UsersService } from '../../services/users.service';
+
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
-
   activeMenu = false;
   counter = 0;
+  token = '';
+  profile: User | null = null;
 
   constructor(
-    private storeService: StoreService
-  ) { }
+    private storeService: StoreService,
+    private authService: AuthService,
+    private usersService: UsersService
+  ) {}
 
   ngOnInit(): void {
-    this.storeService.myCart$.subscribe(products => {
+    this.storeService.myCart$.subscribe((products) => {
       this.counter = products.length;
     });
   }
@@ -26,4 +34,31 @@ export class NavComponent implements OnInit {
     this.activeMenu = !this.activeMenu;
   }
 
+  login() {
+    this.authService.login('vicente@gmail.com', '123123').subscribe((rta) => {
+      this.token = rta.access_token;
+      console.log(rta.access_token);
+      this.getProfile();
+    });
+  }
+
+  loginProfile() {
+    this.authService
+      .login('vicente@gmail.com', '123123')
+      .pipe(
+        switchMap((rta) => {
+          this.token = rta.access_token;
+          return this.authService.profile(this.token);
+        })
+      )
+      .subscribe((data) => {
+        this.profile = data;
+      });
+  }
+
+  getProfile() {
+    this.authService.profile(this.token).subscribe((user) => {
+      this.profile = user;
+    });
+  }
 }
