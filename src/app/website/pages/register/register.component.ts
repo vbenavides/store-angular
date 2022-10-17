aa3b29bddbc8b699e49a47390dce8fb527ce86fa
Vicente Benavides <vicente.benavides0@gmail.com>
2022-10-16 23:35:38 -0500

    agregando guardian CanDeactivate

--- src/app/website/pages/register/register.component.ts
+++ src/app/website/pages/register/register.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { OnExit } from 'src/app/guards/exit.guard';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnExit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onExit() {
+    const rta = confirm('Quieres salir?');
+    return rta;
   }
-
 }
